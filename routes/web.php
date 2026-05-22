507da72f3652a100cdc692c8354744b7d07ea4d7
qunoot zehra <qunootzehra21@gmail.com>
2026-05-22 22:48:16 +0500

    ss

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,8 @@ use App\Http\Controllers\StockAdjustmentController;
     |
     */
 // kashan connection_aborted
+
+// Qunt connected 
 Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
 Route::get('System/Reports', [HomeController::class, 'System_Reports'])->name('System.Reports')->middleware('permission:System Reports');
